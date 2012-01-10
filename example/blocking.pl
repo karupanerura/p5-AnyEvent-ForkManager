5e06422dda283e0ae16cc784dc74ec6f9af8f43c
Kenta Sato <karupa@cpan.org>
2012-01-11 08:47:39 +0900

    add example

--- example/blocking.pl
+++ example/blocking.pl
@@ -1,20 +1,38 @@
+use strict;
+use warnings;
+use utf8;
+
 use AnyEvent::ForkManager;
 
 my $MAX_WORKERS = 10;
 my $pm = AnyEvent::ForkManager->new(max_workers => $MAX_WORKERS);
 
 use List::Util qw/shuffle/;
-my @all_data = shuffle(1 .. 10);
-foreach $data (@all_data) {
+my @all_data = shuffle(1 .. $MAX_WORKERS);
+foreach my $data (@all_data) {
     $pm->start(
         cb => sub {
             my($pm, $data) = @_;
             sleep $data;
             printf("Sleeped %d sec.\n", $data);
+            my $exit_code = $data;
+            printf("  Exit code = %d\n", $exit_code);
+            $pm->finish($exit_code);
         },
         args => [$data]
     );
 }
+
+$pm->on_finish(sub{
+    my($pid, $status, $data) = @_;
+
+    printf("finished child proccess. {pid => %d, status => %d, sleep_time => %d}\n", $pid, $status >> 8, $data);
+});
+$pm->on_error(sub{
+    my($pm, $data) = @_;
+
+    printf("fork failed. on dispatch %d. object => %s.}\n", $data, $pm);
+});
 
 $pm->wait_all_children(
     cb => sub {
--- example/nonblocking.pl
+++ example/nonblocking.pl
@@ -1,3 +1,7 @@
+use strict;
+use warnings;
+use utf8;
+
 use AnyEvent;
 use AnyEvent::ForkManager;
 
@@ -5,17 +9,31 @@ my $MAX_WORKERS = 10;
 my $pm = AnyEvent::ForkManager->new(max_workers => $MAX_WORKERS);
 
 use List::Util qw/shuffle/;
-my @all_data = shuffle(1 .. 10);
-foreach $data (@all_data) {
+my @all_data = shuffle(1 .. $MAX_WORKERS);
+foreach my $data (@all_data) {
     $pm->start(
         cb => sub {
             my($pm, $data) = @_;
             sleep $data;
             printf("Sleeped %d sec.\n", $data);
+            my $exit_code = $data;
+            printf("  Exit code = %d\n", $exit_code);
+            $pm->finish($exit_code);
         },
         args => [$data]
     );
 }
+
+$pm->on_finish(sub{
+    my($pid, $status, $data) = @_;
+
+    printf("finished child proccess. {pid => %d, status => %d, sleep_time => %d}\n", $pid, $status >> 8, $data);
+});
+$pm->on_error(sub{
+    my($pm, $data) = @_;
+
+    printf("fork failed. on dispatch %d. object => %s.}\n", $data, $pm);
+});
 
 my $cv = AnyEvent->condvar;
 $pm->wait_all_children(
@@ -24,6 +42,7 @@ $pm->wait_all_children(
         warn 'called';
         $cv->send;
     },
+    blocking => 1,
 );
 $cv->recv;
 warn 'end';
