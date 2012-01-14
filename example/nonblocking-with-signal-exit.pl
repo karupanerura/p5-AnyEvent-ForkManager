0a63bb6a0da2bb4e72a67237331c98181a556a36
Kenta Sato <karupa@cpan.org>
2012-01-14 15:24:53 +0900

    nonblockingなプログラムでblockingしながらwaitする使用例書いた。

---
+++ example/nonblocking-with-signal-exit.pl
@@ -0,0 +1,59 @@
+use strict;
+use warnings;
+use utf8;
+
+use AnyEvent;
+use AnyEvent::ForkManager;
+
+my $MAX_WORKERS = 10;
+my $pm = AnyEvent::ForkManager->new(max_workers => $MAX_WORKERS);
+
+use List::Util qw/shuffle/;
+my @all_data = shuffle(1 .. $MAX_WORKERS);
+foreach my $data (@all_data) {
+    $pm->start(
+        cb => sub {
+            my($pm, $data) = @_;
+            local $SIG{INT} = sub {};
+
+            my $finish = time + $data;
+            1 while (time <= $finish);
+            printf("Sleeped %d sec.\n", $data);
+
+            my $exit_code = $data;
+            printf("  Exit code = %d\n", $exit_code);
+            $pm->finish($exit_code);
+        },
+        args => [$data]
+    );
+}
+
+$pm->on_finish(sub{
+    my($pid, $status, $data) = @_;
+
+    printf("finished child proccess. {pid => %d, status => %d, sleep_time => %d}\n", $pid, $status >> 8, $data);
+});
+$pm->on_error(sub{
+    my($pm, $data) = @_;
+
+    printf("fork failed. on dispatch %d. object => %s.}\n", $data, $pm);
+});
+
+my $cv = AnyEvent->condvar;
+my $sig = AnyEvent->signal(
+    signal => 'INT',
+    cb     => sub {
+        warn 'start wait';
+        $pm->wait_all_children(
+            cb => sub {
+                my($pm) = @_;
+                warn 'called';
+                warn 'end wait';
+                $cv->send;
+            },
+            blocking => 1
+        );
+    }
+);
+$cv->recv;
+warn 'end';
